53af83f281177fd305737d68227832b4e686e464
Jeff <jlui820@gmail.com>
2021-03-09 15:12:08 -0800

    app cleanup

---
+++ robinhood-clone/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app_header">
+        <Header />
+      </div>
+      <div className="app_body">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
