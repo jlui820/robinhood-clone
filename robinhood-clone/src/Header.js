aaac41fde61493fe993b58e1fc59ac78973c54f0
Jeff <jlui820@gmail.com>
2021-03-09 15:23:51 -0800

    add logo to header component

--- robinhood-clone/src/Header.js
+++ robinhood-clone/src/Header.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Logo from './robinhood.svg';
 
 const Header = () => {
   return (
     <div className='header__wrapper'>
       {/* logo */}
       <div className="header__logo">
-        test
+        <img src={Logo} width={25} />
       </div>
       {/* Search */}
       {/* menuitems */}
