11ff6795e70ba74760d5d6952f8bfe83ba1b6ecb
Jeff <jlui820@gmail.com>
2021-03-10 13:19:32 -0800

    install materialui and icons

--- robinhood-clone/src/Header.js
+++ robinhood-clone/src/Header.js
@@ -4,11 +4,16 @@ import Logo from './robinhood.svg';
 const Header = () => {
   return (
     <div className='header__wrapper'>
-      {/* logo */}
+      
       <div className="header__logo">
         <img src={Logo} width={25} />
       </div>
-      {/* Search */}
+     
+      <div className="header__search">
+        <div className="header__searchContainer">
+
+        </div>
+      </div>
       {/* menuitems */}
     </div>
   )
